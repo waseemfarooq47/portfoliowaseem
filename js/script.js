8d80760042b476f4f8b2053c17ee7980c66f6edd
Waseem Farooq <49933034+waseemfarooq47@users.noreply.github.com>
2024-03-14 17:59:06 +0400

    added mobile navigation

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: '.slider-dots',
+    dotsClass: 'dots'
+  })
+  $('.autoplay').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 200,
+  });
+})
+
+// mobile nav
+
+let hamburger= document.querySelector(".hamburger");
+let times =document.querySelector('.times')
+let mobileNav =document.querySelector(".mobile-nav")
+
+hamburger.addEventListener('click',()=>{
+mobileNav.classList.add('open')
+})
+times.addEventListener('click',()=>{
+  mobileNav.classList.remove('open')
+})
